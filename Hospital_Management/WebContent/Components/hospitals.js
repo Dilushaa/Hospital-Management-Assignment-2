dadf0790312fbc3854d55f1fe2b553262463f972
Dilushaa <dilushagayanebaynew1@gmail.com>
2020-05-04 01:03:31 +0530

    implement a validate form

--- Hospital_Management/WebContent/Components/hospitals.js
+++ Hospital_Management/WebContent/Components/hospitals.js
@@ -1,3 +1,47 @@
-/**
- * 
- */
+$(document).ready(function()
+{
+	
+if ($("#alertSuccess").text().trim() == "")
+ {
+ $("#alertSuccess").hide();
+ }
+ $("#alertError").hide();
+});
+
+
+//SAVE ============================================
+$(document).on("click", "#btnSave", function(event)
+{
+// Clear alerts---------------------
+ $("#alertSuccess").text("");
+ $("#alertSuccess").hide();
+ $("#alertError").text("");
+ $("#alertError").hide();
+ 
+//Form validation-------------------
+ var status = validateHospitalForm();
+ if (status != true)
+  {
+  $("#alertError").text(status);
+  $("#alertError").show();
+  return;
+  }
+ 
+ 
+//If valid use ajax------------------------
+
+ var type = ($("#hidHospitalIDSave").val() == "") ? "POST" : "PUT";
+
+ $.ajax(
+ 		{
+ 		 url : "HospitalsAPI",
+ 		 type : type,
+ 		 data : $("#formHospital").serialize(),
+ 		 dataType : "text",
+ 		 complete : function(response, status)
+ 		 {
+ 		 onHospitalSaveComplete(response.responseText, status);
+ 		 }
+ 		});
+
+ });
