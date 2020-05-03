da035a5b63b46380b2abeb1ad597fc9ee3ed6528
Dilushaa <dilushagayanebaynew1@gmail.com>
2020-05-04 00:57:56 +0530

    convert request parameters to a map

--- Hospital_Management/src/com/HospitalsAPI.java
+++ Hospital_Management/src/com/HospitalsAPI.java
@@ -1,7 +1,9 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -76,5 +78,33 @@ public class HospitalsAPI extends HttpServlet {
 		 
 		 response.getWriter().write(output); 
 	}
+	
+	// Convert request parameters to a Map
+			private static Map getParasMap(HttpServletRequest request)
+			{
+			 Map<String, String> map = new HashMap<String, String>();
+				try
+				 {
+				 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				 String queryString = scanner.hasNext() ?
+				 scanner.useDelimiter("\\A").next() : "";
+				 scanner.close();
+				 String[] params = queryString.split("&");
+					 for (String param : params)
+					 {
+						 String[] p = param.split("=");
+						 map.put(p[0], p[1]);
+					 }
+					 
+				}
+				catch (Exception e)
+				{
+				}
+					
+				return map;
+					
+			}
+		
+		
 
 }
