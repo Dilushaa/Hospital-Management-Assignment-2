ce93e275dea026a10c64ffbf81153bac83275bf0
Dilushaa <dilushagayanebaynew1@gmail.com>
2020-05-03 23:31:22 +0530

    Add db connect class

--- Hospital_Management/src/com/Hospital.java
+++ Hospital_Management/src/com/Hospital.java
@@ -1,5 +1,31 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Hospital {
 
+	public Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1/hospital","root", ""); 
+			
+			//for testing purpose
+			System.out.print("Successfully connected");
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+			
+		}
+		
+		return con;
+		
+		
+	}
+	
+	
+	
 }
